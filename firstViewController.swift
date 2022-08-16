59d7d49cbd822504e8762f7ec9d34028b1529f57
Kayla <onlykzl13@gmail.com>
2022-08-16 13:37:47 -0400

    Initial Commit

---
+++ firstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  firstViewController.swift
+//  MiniProject2-Quiz
+//
+//  Created by Kayla Lam on 8/12/22.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
